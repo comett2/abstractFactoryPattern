c6ae09f621e111e7167feea1e3c6418141dd55a8
Michał Koman <Micha Koman>
2016-01-18 22:10:18 +0100

    Abstract Factory Example

---
+++ src/main/java/cars/Fiat500C.java
@@ -0,0 +1,8 @@
+package cars;
+
+public class Fiat500C extends Car {
+    @Override
+    public void printCarDescription() {
+        System.out.println("I'm tiny");
+    }
+}
---
+++ src/main/java/cars/GranTurismo.java
@@ -0,0 +1,8 @@
+package cars;
+
+public class GranTurismo extends Car {
+    @Override
+    public void printCarDescription() {
+        System.out.println("Fast like a wind");
+    }
+}
